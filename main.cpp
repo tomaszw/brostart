40243f590a3328a0a2aee67149f5d366f2d7a5da
Tomasz Wroblewski <tomasz.wroblewski@hp.com>
2021-11-19 13:26:57 +0100

    more foobar

--- main.cpp
+++ main.cpp
@@ -11,60 +11,133 @@ const CLSID CLSID_ImmersiveShell = {
  
 IVirtualDesktopManager *pDesktopManager = NULL;
 
+static bool browser_shown = false;
+
+struct find_window {
+  find_window() : on_current_desktop(false), on_current_monitor(false), pid(0) { }
+  string class_name;
+  string title_right;
+  bool on_current_desktop;
+  bool on_current_monitor;
+  DWORD pid;
+};
+
+struct enum_windows_param {
+  find_window fw;
+  HWND hwnd;
+};
+
 BOOL CALLBACK enum_windows_cb(HWND hwnd, LPARAM lp)
 {
+  enum_windows_param *p = (enum_windows_param*)lp;
   char buf[1024];
 
-  GetClassNameA(hwnd, &buf[0], sizeof(buf));
+  if (!IsWindowVisible(hwnd))
+    return TRUE;
 
+  GetClassNameA(hwnd, &buf[0], sizeof(buf));
   string cname(buf);
-  if (cname == "MozillaWindowClass") {
+
+  if (p->fw.class_name.size() && p->fw.class_name != cname)
+    return TRUE;
+
+  if (p->fw.title_right.size()) {
     GetWindowTextA(hwnd, &buf[0], sizeof(buf));
     string text(buf);
-    string pat("Mozilla Firefox");
-    if (text.size() >= pat.size()) {
-      if (text.substr(text.size() - pat.size(), pat.size()) == pat) {
-        BOOL on_desktop = FALSE;
-        pDesktopManager->IsWindowOnCurrentVirtualDesktop(hwnd, &on_desktop);
-
-        if (!IsWindowVisible(hwnd) || !on_desktop)
-          return TRUE;
-
-        POINT cursor;
-        GetCursorPos(&cursor);
-
-        WINDOWPLACEMENT pl;
-        GetWindowPlacement(hwnd, &pl);
-        pl.length = sizeof(pl);
-
-        POINT lt_window;
-        lt_window.x = pl.rcNormalPosition.left;
-        lt_window.y = pl.rcNormalPosition.top;
-        HMONITOR mon_window = MonitorFromRect(&pl.rcNormalPosition, MONITOR_DEFAULTTONULL);
-        HMONITOR mon_cursor = MonitorFromPoint(cursor, MONITOR_DEFAULTTONULL);
-
-        if (!mon_window || !mon_cursor || (mon_window != mon_cursor))
-          return TRUE;
-
-        RECT rc = pl.rcNormalPosition;
-        printf("%s\n", text.c_str());
-        printf("%d %d %d %d\n", rc.left, rc.top, rc.right, rc.bottom);
-        if (pl.showCmd == SW_SHOWMAXIMIZED) {
-          ShowWindow(hwnd, SW_SHOWMAXIMIZED);
-        } else if (pl.showCmd == SW_SHOWMINIMIZED) {
-          ShowWindow(hwnd, SW_RESTORE);
-        } else {
-          ShowWindow(hwnd, SW_SHOWNORMAL);
-        }
-        
-        printf("cursor @ %d,%d\n", cursor.x, cursor.y);
-        //BringWindowToTop(hwnd);
-        SetForegroundWindow(hwnd);
-        return FALSE;
-      }
-    }
+    string pat(p->fw.title_right);
+    if (text.size() < pat.size())
+      return TRUE;
+    
+    if (text.substr(text.size() - pat.size(), pat.size()) != pat)
+      return TRUE;
   }
-  return TRUE;
+
+  if (p->fw.on_current_desktop) {
+    BOOL on_desktop = FALSE;
+    pDesktopManager->IsWindowOnCurrentVirtualDesktop(hwnd, &on_desktop);
+    if (!on_desktop)
+      return TRUE;
+  }
+
+  if (p->fw.on_current_monitor) {
+    POINT cursor;
+    GetCursorPos(&cursor);
+
+    WINDOWPLACEMENT pl;
+    GetWindowPlacement(hwnd, &pl);
+    pl.length = sizeof(pl);
+
+    POINT lt_window;
+    lt_window.x = pl.rcNormalPosition.left;
+    lt_window.y = pl.rcNormalPosition.top;
+    HMONITOR mon_window = MonitorFromRect(&pl.rcNormalPosition, MONITOR_DEFAULTTONULL);
+    if (!mon_window)
+      mon_window = MonitorFromPoint(lt_window, MONITOR_DEFAULTTONULL);
+    HMONITOR mon_cursor = MonitorFromPoint(cursor, MONITOR_DEFAULTTONULL);
+
+    if (!mon_window || !mon_cursor || (mon_window != mon_cursor))
+      return TRUE;
+  }
+
+    //printf("%d\n", (int)pid);
+  if (p->fw.pid) {
+    DWORD pid = 0;
+    GetWindowThreadProcessId(hwnd, &pid);
+    if (pid != p->fw.pid)
+      return TRUE;
+  }
+  /* found window */
+  p->hwnd = hwnd;
+  return FALSE;
+}
+
+HWND search(struct find_window fw)
+{
+  enum_windows_param param;
+  param.hwnd = 0;
+  param.fw = fw;
+
+  EnumWindows(enum_windows_cb, (LPARAM) &param);
+
+  return param.hwnd;
+}
+
+static void browser_start()
+{
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+
+  ZeroMemory( &si, sizeof(si) );
+  si.cb = sizeof(si);
+  ZeroMemory( &pi, sizeof(pi) );
+
+  // Start the child process. 
+  if( !CreateProcessA( NULL,   // No module name (use command line)
+      "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe",        // Command line
+      NULL,           // Process handle not inheritable
+      NULL,           // Thread handle not inheritable
+      FALSE,          // Set handle inheritance to FALSE
+      0,              // No creation flags
+      NULL,           // Use parent's environment block
+      NULL,           // Use parent's starting directory 
+      &si,            // Pointer to STARTUPINFO structure
+      &pi )           // Pointer to PROCESS_INFORMATION structure
+  ) 
+  {
+      printf( "CreateProcess failed (%d).\n", GetLastError() );
+      return;
+  }
+
+  find_window fw;
+  fw.pid = pi.dwProcessId;
+
+  //for (;;) {
+    HWND hwnd = search(fw);
+    printf("hwnd=%p\n", hwnd);
+    //if (hwnd)
+      //break;
+    //Sleep(200);
+  //}
 }
 
 int main() {
@@ -84,6 +157,23 @@ int main() {
     exit(1);
   }
 
+  find_window fw;
+  fw.class_name = "MozillaWindowClass";
+  fw.title_right = "Mozilla Firefox";
+  fw.on_current_desktop = true;
+  fw.on_current_monitor = true;
+  fw.pid = 0;
 
-  EnumWindows(enum_windows_cb, NULL);
+  HWND hwnd = search(fw);
+  if (hwnd) {
+    WINDOWPLACEMENT pl;
+    pl.length = sizeof(pl);
+    GetWindowPlacement(hwnd, &pl);
+    if (pl.showCmd == SW_SHOWMINIMIZED) {
+      ShowWindow(hwnd, SW_RESTORE);
+    }
+    SetForegroundWindow(hwnd);
+  } else {
+    browser_start();
+  }
 }
